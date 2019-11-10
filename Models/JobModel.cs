54276466afc9a6ed3c5ebe7d6f607565e0222373
UltimateDadBod <emilvinkel@gmail.com>
2019-11-10 22:28:08 +0100

    JobModel Updated

--- Models/JobModel.cs
+++ Models/JobModel.cs
@@ -9,18 +9,20 @@ namespace Freelance_Api.Models
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
+
         public string CompanyName { get; set; }
         public string Title { get; set; }
         public int Salary { get; set; }
         public string Location { get; set; }
         public string Description { get; set; }
         public string[] Tags { get; set; }
-        public bool PaidMonthly { get; set; } 
-        public bool PaidHourly { get; set; } 
+        public bool PaidMonthly { get; set; }
+        public bool PaidHourly { get; set; }
         public string Experience { get; set; }
+        public int Freelancers { get; set; }
+        public DateTime Deadline { get; set; }
+        public DateTime CreatedOn { get; set; }
         public DateTime JobStart { get; set; }
         public DateTime JobEnd { get; set; }
-
-        
     }
 }
